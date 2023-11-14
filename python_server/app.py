f67da3fd4703f749633570674de2d110597abc73
Ubuntu <ubuntu@ip-172-31-6-197.ap-northeast-2.compute.internal>
2023-11-14 12:20:38 +0000

    Init Flask application app.py
    - Minimum HTTP POST API implemented Python Flaks application

---
+++ python_server/app.py
@@ -0,0 +1,29 @@
+import io
+
+from flask import request, Flask, send_file, Response
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    return 'Flask Application to return recommend image'
+
+
+def compose_images(input_img: bytes, wannabe_img: bytes) -> bytes:
+    return bytes(b'output_img')
+
+
+@app.route('/recommend', methods=['POST'])
+def handle_request() -> Response:
+    input_img = request.files['img_file'].read()
+    wannabe_img = None
+    output_img = compose_images(input_img, wannabe_img)
+
+    return send_file(io.BytesIO(output_img),
+                     download_name='composition_image')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)

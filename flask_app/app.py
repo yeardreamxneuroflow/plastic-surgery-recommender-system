a9f9d786bdde78a2ed72f628d4f3d25c7c2a2fe6
Yi Hong-ju <to_hongju@pm.me>
2023-11-21 06:29:57 +0000

    Update Flask Application `app.py`
    New module/method naming, construction

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,15 @@
 import io
 
-from flask import request, Flask, send_file, Response
+from werkzeug.datastructures.file_storage import FileStorage
+from flask import Flask
+from flask import Response
+from flask import request
+from flask import send_file
+
+from face_landmark import get_face_landmark_img
+from image_vectorization import vectorize_img
+from wannabe_image import get_most_similar_wannabe
+from image_generation import generate_image
 
 
 app = Flask(__name__)
@@ -8,21 +17,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index() -> str:
-    return 'Flask Application to return recommend image'
-
-
-def compose_images(input_img: bytes, wannabe_img: bytes) -> bytes:
-    return input_img  # Temporary return value
+    return 'Flask Application to return recommend image...'
 
 
 @app.route('/recommend', methods=['POST'])
 def handle_request() -> Response:
-    input_img = request.files['img_file'].read()
-    wannabe_img = None
-    output_img = compose_images(input_img, wannabe_img)
+    input_img: FileStorage = request.files['img_file']
+    face_landmark_img: list = get_face_landmark_img(input_img)
+    vectorized_face_landmakrs = vectorize_img(face_landmark_img)
+    most_similar_wannabe_img = get_most_similar_wannabe(
+        vectorized_face_landmakrs
+    )
+    output_img = generate_image(input_img, most_similar_wannabe_img)
 
-    return send_file(io.BytesIO(output_img),
-                     download_name='composition_image')
+    return send_file(
+        io.BytesIO(output_img),
+        download_name='output_img_file'
+    )
 
 
 if __name__ == '__main__':

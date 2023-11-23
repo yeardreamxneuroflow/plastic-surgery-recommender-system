f594bb9ecacab01af08086ead711c95a60283abd
Yi Hong-ju <to_hongju@pm.me>
2023-11-23 10:45:25 +0000

    Refactor Flask 'app.py'
    - Modify module import method
    - Import 'PIL' for type hinting
    - Modify variable/function names

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,14 +1,13 @@
 import io
 
 from werkzeug.datastructures.file_storage import FileStorage
-from flask import Flask
-from flask import Response
-from flask import request
-from flask import send_file
+from flask import Flask, Response, request, send_file
 
-from face_landmark import get_face_landmark_img
+from PIL import Image
+
+from face_landmark import get_face_landmark_imgs
 from image_vectorization import vectorize_img
-from wannabe_image import get_most_similar_wannabe
+from wannabe_image import get_most_similar_wannabe_img
 from image_generation import generate_image
 
 
@@ -23,10 +22,10 @@ def index() -> str:
 @app.route('/recommend', methods=['POST'])
 def handle_request() -> Response:
     input_img: FileStorage = request.files['img_file']
-    face_landmark_img: list = get_face_landmark_img(input_img)
-    vectorized_face_landmakrs = vectorize_img(face_landmark_img)
-    most_similar_wannabe_img = get_most_similar_wannabe(
-        vectorized_face_landmakrs
+    face_landmark_imgs: list[Image] = get_face_landmark_imgs(input_img)
+    vectorized_landmakrs = vectorize_img(face_landmark_imgs)
+    most_similar_wannabe_img = get_most_similar_wannabe_img(
+        vectorized_landmakrs
     )
     output_img = generate_image(input_img, most_similar_wannabe_img)
 

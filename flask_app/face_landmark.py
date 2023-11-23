8a376ef416ee369dc595e710c2e4c128eb8bd476
Yi Hong-ju <to_hongju@pm.me>
2023-11-23 12:41:58 +0000

    Update 'face_landmark.py'
    - Modify import of `PIL.Image`
    - Import 'face_landmark_macro'
    - Build `denormalize_landmark_points()`
    - Build `detect_landmark_points()`
    - Build `get_single_landmark_img()`
    - Modify `get_face_landmark_img()`
    	- `get_face_landmark_imgs()`
    	- Renew main logic

--- flask_app/face_landmark.py
+++ flask_app/face_landmark.py
@@ -5,19 +5,30 @@ from mediapipe.tasks.python import BaseOptions
 from mediapipe.tasks.python import vision
 from mediapipe.tasks.python.components.containers.landmark import NormalizedLandmark
 
+import PIL
 import numpy as np
-from PIL import Image
+
+import face_landmark_macro
 
 
-def get_img_pieces(
-    input_img: FileStorage,
-    result_landmarks: list[list[NormalizedLandmark]],
-    landmarks_to_extract: list,
-) -> list:
-    return []
+def denormalize_landmark_points(
+    pil_img: PIL.Image,
+    points_to_denormalize: list[NormalizedLandmark],
+) -> None:
+    img_width, img_height = pil_img.size
+
+    for _, landmark in enumerate(points_to_denormalize):
+        landmark.x = landmark.x * img_width
+        landmark.y = landmark.y * img_height
 
 
-def get_face_landmark_img(input_img: FileStorage) -> list:
+def detect_landmark_points(
+    pil_img: PIL.Image,
+) -> list[NormalizedLandmark]:
+    mp_img = np.array(pil_img)
+    mp_img = mp.Image(image_format=mp.ImageFormat.SRGB,
+                      data=mp_img)
+
     options = vision.FaceLandmarkerOptions(
         base_options=BaseOptions(
             model_asset_path='./tasks/face_landmarker.task'
@@ -25,29 +36,82 @@ def get_face_landmark_img(input_img: FileStorage) -> list:
         num_faces=1,
     )
     detector = vision.FaceLandmarker.create_from_options(options)
+    detection_result: vision.FaceLandmarkerResult = detector.detect(mp_img)
 
-    # Image converting sequence
-    # `input.img` -> PIL -> Numpy -> MediaPipe -> `converted_input_img`
-    converted_input_img = Image.open(input_img.stream)
-    converted_input_img = np.array(converted_input_img)
-    converted_input_img = mp.Image(
-        image_format=mp.ImageFormat.SRGB, data=converted_input_img
-    )
+    denormalize_landmark_points(pil_img, detection_result.face_landmarks[0])
 
-    detection_result: vision.FaceLandmarkerResult = detector.detect(
-        converted_input_img
-    )
-    face_landmark_img: list = get_img_pieces(
-        input_img=input_img,
-        result_landmarks=detection_result.face_landmarks,
-        landmarks_to_extract=[
-            mp.solutions.face_mesh.FACEMESH_LEFT_EYE,
-            mp.solutions.face_mesh.FACEMESH_RIGHT_EYE,
-            mp.solutions.face_mesh.FACEMESH_NOSE,
-            mp.solutions.face_mesh.FACEMESH_LIPS,
-            mp.solutions.face_mesh.FACEMESH_CONTOURS,
-            mp.solutions.face_mesh.FACEMESH_IRISES,
-        ],
-    )
+    return detection_result.face_landmarks[0]
 
-    return face_landmark_img
+
+def get_single_landmark_img(
+    img: PIL.Image,
+    denormalized_landmark_points: list[NormalizedLandmark],
+    x1_idx: int,
+    y1_idx: int,
+    x2_idx: int,
+    y2_idx: int,
+) -> PIL.Image:
+    copied_img_to_crop = img.copy()
+    x1: float = denormalized_landmark_points[x1_idx].x
+    y1: float = denormalized_landmark_points[y1_idx].y
+    x2: float = denormalized_landmark_points[x2_idx].x
+    y2: float = denormalized_landmark_points[y2_idx].y
+
+    return copied_img_to_crop.crop((x1, y1, x2, y2))
+
+
+def get_face_landmark_imgs(input_img: FileStorage) -> list[PIL.Image]:
+    """
+    Kind of face landmarks
+    - Left eye
+    - Right eye
+    - Nose
+    - Lips
+    """
+
+    pil_img = PIL.Image.open(input_img.stream)
+    detected_landmark_points = detect_landmark_points(pil_img)
+
+    left_eye_img = get_single_landmark_img(
+        pil_img,
+        detected_landmark_points,
+        face_landmark_macro.LEFT_EYE_RIGHT_EDGE,
+        face_landmark_macro.LEFT_EYE_TOP_EDGE,
+        face_landmark_macro.LEFT_EYE_LEFT_EDGE,
+        face_landmark_macro.LEFT_EYE_BOTTOM_EDGE,
+    )
+    right_eye_img = get_single_landmark_img(
+        pil_img,
+        detected_landmark_points,
+        face_landmark_macro.RIGHT_EYE_RIGHT_EDGE,
+        face_landmark_macro.RIGHT_EYE_TOP_EDGE,
+        face_landmark_macro.RIGHT_EYE_LEFT_EDGE,
+        face_landmark_macro.RIGHT_EYE_BOTTOM_EDGE,
+    )
+    nose_img = get_single_landmark_img(
+        pil_img,
+        detected_landmark_points,
+        face_landmark_macro.NOSE_LEFT_EDGE,
+        face_landmark_macro.NOSE_TOP_EDGE,
+        face_landmark_macro.NOSE_RIGHT_EDGE,
+        face_landmark_macro.NOSE_BOTTOM_EDGE,
+    )
+    lips_img = get_single_landmark_img(
+        pil_img,
+        detected_landmark_points,
+        face_landmark_macro.LIPS_LEFT_EDGE,
+        face_landmark_macro.LIPS_TOP_EDGE,
+        face_landmark_macro.LIPS_RIGHT_EDGE,
+        face_landmark_macro.LIPS_BOTTOM_EDGE,
+    )
+    # # Idea: Extract 'face oval'(얼굴윤곽)
+    # face_oval_img: Image = get_single_landmark_img(
+    #     pil_input_img,
+    #     result_landmarks,
+    #     face_landmark_macro.FACE_OVAL_LEFT_EDGE,
+    #     face_landmark_macro.FACE_OVAL_RIGHT_EDGE,
+    #     face_landmark_macro.FACE_OVAL_TOP_EDGE,
+    #     face_landmark_macro.FACE_OVAL_BOTTOM_EDGE,
+    # )
+
+    return [left_eye_img, right_eye_img, nose_img, lips_img]

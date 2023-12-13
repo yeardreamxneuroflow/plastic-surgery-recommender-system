580867d0cac316b2ad811ed8227876a465f2fe1f
Yi Hong-ju <to_hongju@pm.me>
2023-12-13 01:39:23 +0000

    Delete Old Image Processing Logics, Modules
    Modules and codes
    - image_vectorization.py
    - wannabe_image.py
    - image_generation.py
    
    Delete development running code too.
    Replace running to CLI

--- flask-app/app.py
+++ flask-app/app.py
@@ -6,9 +6,6 @@ from flask import Flask, Response, request, send_file
 import PIL
 
 from face_landmark import get_face_landmark_imgs
-from image_vectorization import vectorize_imgs
-from wannabe_image import get_most_similar_wannabe_img
-from image_generation import generate_image
 
 
 app = Flask(__name__)
@@ -23,17 +20,8 @@ def index() -> str:
 def handle_request() -> Response:
     input_img: FileStorage = request.files['img_file']
     face_landmark_imgs: list[PIL.Image] = get_face_landmark_imgs(input_img)
-    vectorized_landmarks = vectorize_imgs(face_landmark_imgs)
-    most_similar_wannabe_img = get_most_similar_wannabe_img(
-        vectorized_landmarks
-    )
-    output_img = generate_image(input_img, most_similar_wannabe_img)
 
     return send_file(
         io.BytesIO(output_img),
         download_name='output_img_file'
     )
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)

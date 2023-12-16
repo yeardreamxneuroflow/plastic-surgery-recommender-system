4adbea6b6d93d714fdfbadee950b1ee5fce3267d
Yi Hong-ju <to_hongju@pm.me>
2023-12-16 10:48:11 +0000

    Update `/recommend` API and `app.py`
    - Add docstring, processing logic, TODO comment, etc.
    - Comment unused modules
    - Add new custom method
    - Update return tag of `/`(root path)

--- flask-apis/app.py
+++ flask-apis/app.py
@@ -1,11 +1,13 @@
-import io
+# import io
 
 from werkzeug.datastructures.file_storage import FileStorage
-from flask import Flask, Response, request, send_file
+# from flask import Flask, Response, request, send_file
+from flask import Flask, Response, request
 
 import PIL
 
 from face_landmark import get_face_landmark_imgs
+from control_marqo import infer_landmarks
 
 
 app = Flask(__name__)
@@ -13,15 +15,38 @@ app = Flask(__name__)
 
 @app.route('/')
 def index() -> str:
-    return 'Flask Application to return recommend image...'
+    return "<h1>Welcome to Plastic Surgery Recommendation Service Flask \
+Application API!</h1>"
 
 
 @app.route('/recommend', methods=['POST'])
-def handle_request() -> Response:
-    input_img: FileStorage = request.files['img_file']
-    face_landmark_imgs: list[PIL.Image] = get_face_landmark_imgs(input_img)
+def handle_spring_request() -> Response:
+    """Process User Face Image as an Input, Return Multiple Data as an Output
 
-    return send_file(
-        io.BytesIO(output_img),
-        download_name='output_img_file'
-    )
+    Returns:
+        Data Form:
+        [
+            "landmark_00": {
+                1. User's face landmark image
+                2. Most similar wannabe
+                3. Similarity score
+            },
+            "landmark_01": {
+                1. User's face landmark image
+                2. Most similar wannabe
+                3. Similarity score
+            },
+            ...
+        ]
+    """
+
+    # Get user's face landmarks
+    user_img: FileStorage = request.files["img_file"]
+    face_landmark_imgs: list[PIL.Image] = get_face_landmark_imgs(user_img)
+
+    # Infer(search) user landmarks to use marqo
+    inference_result: list[list[str, str]] = infer_landmarks(
+        face_landmark_imgs)
+
+    # TODO: Return data to use Flask module e.g. Reponse, send_file, etc.
+    # return send_file()

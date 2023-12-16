1c460f5d46b31ea281bf225c12197641afaa6b03
Yi Hong-ju <to_hongju@pm.me>
2023-12-16 10:51:29 +0000

    Fix Hard Coding Issue within Scraper Macro File
    - Delete hard coded IP address, port
    - Add new IP address gain logic using `os.getenv()`
    - Add `assert` keyword to raise None value issue

--- flask-apis/scrapy_macro.py
+++ flask-apis/scrapy_macro.py
@@ -1,2 +1,12 @@
-SCRAPER_NODE_IP = "15.164.118.216"
-PORT = 5000
+"""Defined Macros to Operate Image Scraper-related Operations
+"""
+
+
+import os
+
+
+SCRAPER_NODE_IP = os.getenv("SCRAPER_NODE_IP")
+SCRAPER_NODE_PORT = os.getenv("SCRAPER_NODE_PORT")
+
+assert SCRAPER_NODE_IP is not None, "`SCRAPER_NODE_IP` Environment Variable is `None`."
+assert SCRAPER_NODE_PORT is not None, "`SCRAPER_NODE_PORT` Environment Variable is `None`."

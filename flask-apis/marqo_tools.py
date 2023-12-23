a0642e76fcda8d82c53a9ce2ca66c3165bb2def6
Yi Hong-ju <to_hongju@pm.me>
2023-12-23 08:01:28 +0000

    Build `insert_landmarks_to_marqo()` Method
    Build the method w/ deletion of `insert_landmarks_manually()` method
    Move some logics from `insert_landmarks_manually()` method to others including `insert_landmarks_to_marqo()`
    
    TODO: Replace macro-related logics to new paradigm

--- flask-apis/marqo_tools.py
+++ flask-apis/marqo_tools.py
@@ -1,78 +1,50 @@
-from PIL import Image
+from typing import List
 
 import marqo
+import boto3
 
+from face_landmark_macro import DEFINED_LANDMARKS
 from marqo_macro import VECTOR_DB_IP, VECTOR_DB_PORT
+from aws_macro import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from aws_macro import AWS_MANUAL_WANNABE_BUCKET_NAME, S3_URL_EXPIRATION
 
 
-def insert_landmarks_manually():
-    wannabe_list = [
-        "박보검",
-        "뷔",
-        "서강준",
-        "송강",
-        "송중기",
-        "신세경",
-        "아이린",
-        "아이유",
-        "오연서",
-        "윈터",
-        "육성재",
-        "윤아",
-        "이주빈",
-        "장원영",
-        "정국",
-        "정우성",
-        "제니",
-        "차은우",
-        "카리나",
-    ]
-    wannabe_landmark_imgs = []
+def insert_landmarks_to_marqo(wannabe_list: List[str]) -> None:
+    """Insert Extracted Landmarks from S3 Bucket to Marqo
+    """
 
-    # Read landmark images from local
-    for wannabe in wannabe_list:
-        four_landmarks = []
-        for landmark in ["left-eye", "right-eye", "nose", "lips"]:
-            with Image.open(f"manual_images/{wannabe}/{landmark}.jpg") as im:
-                four_landmarks.append(im)
-        wannabe_landmark_imgs.append(four_landmarks)
-
-    insert_landmarks_to_marqo(
-        wannabe_list,
-        wannabe_landmark_imgs,
+    mq = marqo.Client(f"http://{VECTOR_DB_IP}:{VECTOR_DB_PORT}")
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name="ap-northeast-2",
     )
 
-
-def insert_landmarks_to_marqo(
-    wannabe_list: list[str],
-    wannabe_landmark_imgs: list[list[Image]],
-):
-    mq = marqo.Client(f"http://{VECTOR_DB_IP}:{VECTOR_DB_PORT}")
-
-    for wannabe_idx, wannabe in enumerate(wannabe_list):
-        for img_idx, img in enumerate(wannabe_landmark_imgs[wannabe_idx]):
-            if img_idx == 0:
-                landmark = "left-eye"
-            elif img_idx == 1:
-                landmark = "right-eye"
-            elif img_idx == 2:
-                landmark = "nose"
-            elif img_idx == 3:
-                landmark = "lips"
-
-            # TODO: Fix `TypeError: Object of type JpegImageFile is not JSON serializable`
-            mq.index(landmark).add_documents(
+    for wannabe in wannabe_list:
+        for landmark in DEFINED_LANDMARKS:
+            # landmark_path = f"s3://{AWS_MANUAL_WANNABE_BUCKET_NAME}/{wannabe}/{landmark}.jpg"
+            img_url = s3.generate_presigned_url(
+                ClientMethod="get_object",
+                Params={
+                    "Bucket": AWS_MANUAL_WANNABE_BUCKET_NAME,
+                    "Key": f"{wannabe}/{landmark}.jpg",
+                },
+                ExpiresIn=S3_URL_EXPIRATION,
+            )
+            r = mq.index(landmark).add_documents(
                 [
                     {
-                        "img": img,
+                        "img": img_url,
                         "wannabe_is": wannabe,
                     },
                 ],
                 tensor_fields=["img"],
             )
+            r  # BP
 
 
-def infer_landmarks(face_landmark_imgs: list[Image]) -> list[str]:
+def infer_landmarks(face_landmark_imgs: list) -> list[str]:
     # ret = [
     #     [
     #         wannabe,
@@ -97,6 +69,27 @@ def infer_landmarks(face_landmark_imgs: list[Image]) -> list[str]:
         r  # Currently blocked by for manual landmark insertion
 
 
-# Manual Image Data Insertion Only
+# Manual Image Data Insertion
 if __name__ == "__main__":
-    insert_landmarks_manually()
+    wannabe_list = [
+        "박보검",
+        "뷔",
+        "서강준",
+        "송강",
+        "송중기",
+        "신세경",
+        "아이린",
+        "아이유",
+        "오연서",
+        "윈터",
+        "육성재",
+        "윤아",
+        "이주빈",
+        "장원영",
+        "정국",
+        "정우성",
+        "제니",
+        "차은우",
+        "카리나",
+    ]
+    insert_landmarks_to_marqo(wannabe_list)

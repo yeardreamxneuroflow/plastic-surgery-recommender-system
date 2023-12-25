d5d412396ff3a333b6553d1dd6998b39ee60d743
Yi Hong-ju <to_hongju@pm.me>
2023-12-25 09:00:06 +0000

    Add `upload_face_landmark_imgs()` within `app.py`

--- flask-apis/app.py
+++ flask-apis/app.py
@@ -2,8 +2,11 @@
 """
 
 
+import io
+import time
 from typing import List
 
+import boto3
 from flask import Flask, Response, request
 from werkzeug.datastructures import FileStorage
 
@@ -11,6 +14,7 @@ from PIL import Image
 
 from mediapipe_tools import get_face_landmark_imgs
 from marqo_tools import infer_landmarks
+from macro import AWSMacro
 
 
 app = Flask(__name__)
@@ -31,6 +35,42 @@ def handle_scrape_request() -> Response:
 
     # TODO: Run Scrapy to run scrape-store pipeline
     pass
+
+
+def upload_face_landmark_imgs(
+    user_timestamp: str,
+    face_landmark_imgs: List[Image.Image],
+) -> None:
+    """Store extracted landmark images to S3 Bucket
+    """
+
+    aws_macro = AWSMacro()
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=aws_macro.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=aws_macro.AWS_SECRET_ACCESS_KEY,
+    )
+    for landmark_idx, landmark_img in enumerate(face_landmark_imgs):
+        if landmark_idx == 0:
+            landmark = "left-eye"
+        elif landmark_idx == 1:
+            landmark = "right-eye"
+        elif landmark_idx == 2:
+            landmark = "nose"
+        elif landmark_idx == 3:
+            landmark = "lips"
+
+        img_buffer = io.BytesIO()
+        landmark_img.save(img_buffer, format="JPEG")
+        img_buffer.seek(0)
+
+        # Store image to S3 Bucket
+        s3.upload_fileobj(
+            Fileobj=img_buffer,
+            Bucket=aws_macro.AWS_USER_LANDMARK_BUCKET_NAME,
+            Key=f"{user_timestamp}/{landmark}.jpg",
+        )
 
 
 @app.route('/recommend', methods=['POST'])
@@ -58,8 +98,12 @@ def handle_spring_request() -> Response:
     user_img: FileStorage = request.files["img_file"]
     face_landmark_imgs: List[Image.Image] = get_face_landmark_imgs(user_img)
 
+    # TODO: Replace user_timestamp(Unix Timestamp) to managed user ID
+    user_timestamp = str(time.time())
+    upload_face_landmark_imgs(user_timestamp, face_landmark_imgs)
+
     # Searching user landmarks from marqo
-    inference_result: List[List[str, str]] = infer_landmarks(
-        face_landmark_imgs)
+    inference_result: List[List[str, str]] = infer_landmarks(user_timestamp)
+    pass
 
     # TODO: Return multiple data to use Flask module

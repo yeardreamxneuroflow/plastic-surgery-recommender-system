b38b9befc00db0b75876b1e60c60e02849363936
Yi Hong-ju <to_hongju@pm.me>
2023-12-25 09:06:31 +0000

    Update infer_landmarks() method
    By using S3 URL, there is no need to use landmark images on memory.
    Add some macro logics to use S3 URL and generalized logics.
    
    TODO: Delete redefinition within infer_landmarks() method.
    That is temporary used for delayed user landmark image uploading.

--- flask-apis/marqo_tools.py
+++ flask-apis/marqo_tools.py
@@ -50,26 +50,38 @@ def insert_landmarks_to_marqo(wannabe_list: List[str]) -> None:
             r  # Breakpoint
 
 
-def infer_landmarks(face_landmark_imgs: List[Image.Image]) -> List[str]:
+def infer_landmarks(user_timestamp: str) -> List[str]:
     """Get most similar wannabe landmark per each landmark.
     """
 
     mq_macro = MarqoMacro()
+    aws_macro = AWSMacro()
+    mp_macro = MediaPipeMacro()
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=aws_macro.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=aws_macro.AWS_SECRET_ACCESS_KEY,
+        region_name="ap-northeast-2",
+    )
 
     mq = marqo.Client(
         f"http://{mq_macro.VECTOR_DB_IP}:{mq_macro.VECTOR_DB_PORT}")
 
-    for idx, img in enumerate(face_landmark_imgs):
-        if idx == 0:
-            landmark = "left-eye"
-        elif idx == 1:
-            landmark = "right-eye"
-        elif idx == 2:
-            landmark = "nose"
-        elif idx == 3:
-            landmark = "lips"
+    for landmark in mp_macro.Landmark.DEFINED_LANDMARKS:
+        # TODO: DELETE REDEFINITION AFTER TEST
+        user_timestamp = "1703491543.5945203"
+        landmark_img_url = s3.generate_presigned_url(
+            ClientMethod="get_object",
+            Params={
+                "Bucket": aws_macro.AWS_USER_LANDMARK_BUCKET_NAME,
+                "Key": f"{user_timestamp}/{landmark}.jpg",
+            },
+            ExpiresIn=aws_macro.S3_URL_EXPIRATION,
+        )
 
-        r = mq.index(landmark).search(q=img)
+        # TODO: Fix `MarqoWebError`
+        r = mq.index(landmark).search(q=landmark_img_url)
+        pass
 
         # TODO: Process `r` and return result that processed from `r`
 
